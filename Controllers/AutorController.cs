e8c5fab9af2fa2b6fe62ca227dcb41eee9145b80
Lue <lue.amaral@etec.sp.gov.br>
2024-08-28 19:42:33 -0300

    bug fix, cadautor

--- Controllers/AutorController.cs
+++ Controllers/AutorController.cs
@@ -1,13 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProjetoAppLivraria.Models;
 using ProjetoAppLivraria.Repository.Contract;
+using Microsoft.Extensions.Logging;
 
 namespace ProjetoAppLivraria.Controllers
 {
     public class AutorController : Controller
     {
         private readonly ILogger<AutorController> _logger;
-        private IAutorRepository _autorRepository;
+        private readonly IAutorRepository _autorRepository;
 
         public AutorController(ILogger<AutorController> logger, IAutorRepository autorRepository)
         {
@@ -20,18 +21,29 @@ namespace ProjetoAppLivraria.Controllers
             return View(_autorRepository.ObterTodosAutores());
         }
 
-        public IActionResult CadAutor(Autor autor)
-        {
-            _autorRepository.CadastrarAutor(autor);
-            return View();
-        }
-
         public IActionResult EditarAutor(int id)
         {
             var autor = _autorRepository.ObterAutor(id);
             if (autor == null)
             {
                 return NotFound();
+            }
+            return View(autor);
+        }
+
+        [HttpGet]
+        public IActionResult CadAutor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CadAutor(Autor autor)
+        {
+            if (ModelState.IsValid)
+            {
+                _autorRepository.CadastrarAutor(autor); // Usando o método do repositório
+                return RedirectToAction(nameof(Index));
             }
             return View(autor);
         }
@@ -46,6 +58,7 @@ namespace ProjetoAppLivraria.Controllers
             }
             return View(autor);
         }
+
         public IActionResult ExcluirAutor(int id)
         {
             var autor = _autorRepository.ObterAutor(id);
